ec18f0efa43a2219d2fbcc6e73966cd1b4441dbf
noi <noidor2019@gmail.com>
2024-09-18 01:55:00 +0700

    23 current  2024-09-18 01:53:28  24.05.20240830.6e99f2a   6.6.48                          *

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,7 @@
 
   networking.hostName = "nixos"; # Define your hostname.
   networking.networkmanager.enable = true;
-  networking.nameservers = ["1.1.1.1" "8.8.8.8"];
+  networking.nameservers = ["8.8.8.8"];
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   time.timeZone = "Asia/Ho_Chi_Minh";
