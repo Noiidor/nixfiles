9a2ef469815b115f2a74051f384dcf88a77156ef
noi <noidor2019@gmail.com>
2025-02-01 02:39:34 +0700

    Added basic zsh config with plugins

--- modules/zsh/zsh.nix
+++ modules/zsh/zsh.nix
@@ -9,8 +9,12 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    enableVteIntegration = true;
+    defaultKeymap = "emacs";
+
     shellAliases = {
       ez = "eza -a --icons=auto";
+      ls = "ls --color";
       ff = "fastfetch";
       nv = "nvim";
       yz = "yazi";
@@ -26,10 +30,49 @@
       ai = "mods";
     };
 
-    enableVteIntegration = true;
+    plugins = [
+      {
+        name = pkgs.zsh-fzf-tab.pname;
+        src = pkgs.zsh-fzf-tab.src;
+        file = "${pkgs.zsh-fzf-tab.src}/fzf-tab.plugin.zsh";
+      }
+      {
+        name = pkgs.zsh-powerlevel10k.pname;
+        src = pkgs.zsh-powerlevel10k.src;
+        file = "${pkgs.zsh-powerlevel10k.src}/powerlevel10k.zsh-theme";
+      }
+    ];
+
+    history = {
+      size = 5000;
+      save = 5000;
+      share = true;
+      append = true;
+      ignoreSpace = true;
+      ignoreDups = true;
+      ignoreAllDups = true;
+    };
+
+    historySubstringSearch = {
+      enable = true;
+    };
 
     initExtra = ''
-      PROMPT="%F{blue}%~ %(?.%F{green}.%F{red})%#%f "
+      bindkey "^R" history-incremental-pattern-search-backward
+      bindkey '^[OA' history-substring-search-up
+      bindkey '^[OB' history-substring-search-down
+
+      eval "$(fzf --zsh)"
+
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      zstyle ':completion:*' list-colors "$\{(s.:.)LS_COLORS}"
+      zstyle ':completion:*' menu no
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+      zstyle ':fzf-tab:complete:z:*' fzf-preview 'ls --color $realpath'
+
+      source ${pkgs.zsh-fzf-tab.src}/fzf-tab.plugin.zsh
+      source ${pkgs.zsh-powerlevel10k.src}/powerlevel10k.zsh-theme
+      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
     '';
   };
 }
