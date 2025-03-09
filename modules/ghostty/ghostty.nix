87c53e56a537d634b3aedd72fe31ab2766785eb8
noi <noidor2019@gmail.com>
2025-03-09 20:22:18 +0700

    Addec ghostty options

--- modules/ghostty/ghostty.nix
+++ modules/ghostty/ghostty.nix
@@ -7,7 +7,10 @@
         theme = "Hardcore";
         font-family = "JetBraintsMono Nerd Font Mono";
         custom-shader = "/home/${user}/shader.glsl";
+        class = "ghostty";
         gtk-titlebar = false;
+        gtk-tabs-location = "right";
+        gtk-wide-tabs = "false";
       };
     };
   };
