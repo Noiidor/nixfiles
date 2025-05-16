0e85f3509de720450a1f03aa9b7b5f2a8b6bd9c4
noi <noidor2019@gmail.com>
2025-05-16 19:19:26 +0300

    Added flakes templates

---
+++ templates/dev-shell-flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Basic Dev shell";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        # Deps and libs
+      ];
+
+      shellHook = ''
+        echo "Welcome to Shell!"
+      '';
+
+      env = {
+      };
+    };
+  };
+}
---
+++ templates/fhs-env-flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Basic FHS env";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default =
+      (pkgs.buildFHSEnv
+        {
+          name = "fhs-env";
+          targetPkgs = pkgs: (with pkgs; [
+            # Libs
+          ]);
+          runScript = "$SHELL";
+        })
+      .env;
+  };
+}
