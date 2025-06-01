7a49976c15247a22392c88e18c387688b8049a73
noi <noidor2019@gmail.com>
2025-06-01 18:28:35 +0300

    Updated and switched to 25.05 stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "System flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
     home-manager = {
