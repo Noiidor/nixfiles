937a478e403730ea7529f44aa25e24e33813bea2
noi <noidor2019@gmail.com>
2025-06-24 12:04:19 +0300

    Revert "Changed DNS"
    This reverts commit 9543dfc3ec8c4ba57f716d26ef9a57127e10ad61.

--- configuration.nix
+++ configuration.nix
@@ -91,7 +91,7 @@
       0.0.0.0 zzz-log-upload-os.hoyoverse.com
     '';
     #networkmanager.dns = "none";
-    nameservers = ["83.220.169.155" "212.109.195.93"];
+    # nameservers = ["8.8.8.8" "8.8.4.4" "1.1.1.1" "1.0.0.1"];
 
     firewall = {
       enable = true;
