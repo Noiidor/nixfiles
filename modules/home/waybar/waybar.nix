8f5dea9fa5d75ce1acf60515f52e946cb51caf3a
noi <noidor2019@gmail.com>
2025-08-11 12:24:32 +0300

    Added waybar styles

--- modules/home/waybar/waybar.nix
+++ modules/home/waybar/waybar.nix
@@ -1,7 +1,10 @@
 {pkgs, ...}: {
-  programs = {
-    waybar = {
-      enable = true;
-    };
+  programs.waybar = {
+    enable = true;
+  };
+
+  xdg.configFile.waybar = {
+    recursive = true;
+    source = ./.;
   };
 }
